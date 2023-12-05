6a3426f6548acfad45f3c6f3aca218c5a4ebd1cf
sihle <sihle@SPHE-LAPTOP>
2023-12-05 18:17:55 +0200

    Add project files.

---
+++ Designing/Controllers/homepageController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Designing.Controllers
+{
+    public class homepageController : Controller
+    {
+        // GET: homepage
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult homepage() 
+        { 
+            return View();
+        }
+        
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Land()
+        {
+            return View();
+        }
+
+        public ActionResult Category()
+        {
+            return View();
+        }
+
+        public ActionResult Livestock() 
+        {
+            return View();
+        }
+    }
+}
